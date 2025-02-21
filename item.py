201ced336c660af2344d094a5ad8852a51393717
Thomas GLR <thomasguillier@hotmail.fr>
2025-02-21 12:30:49 +0100

    Initialisation projet + récupération des informations dans les fichiers

---
+++ item.py
@@ -0,0 +1,5 @@
+class Item:
+    def __init__(self, id, profit, weight):
+        self.id = id
+        self.profit = profit
+        self.weight = weight
---
+++ magasin.py
@@ -0,0 +1,4 @@
+class Magasin:
+    def __init__(self, capacity, items):
+        self.capacity = capacity
+        self.items = items
